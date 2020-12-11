a4062c4e8674c930e0928df94147fa178588cc82
shannon <shanno330@yahoo.com>
2020-12-10 21:32:53 -0800

    changes2

--- script.js
+++ script.js
@@ -1,25 +1,31 @@
 
-let budgetButton = document.querySelector('.submitBudget');
-let budgetA = document.querySelector(".budgetAmount" );
-let budgetEdit = document.querySelector(".editBudget");
-let expensesAmount= document.querySelector('.purchased')
+// let budgetButton = document.querySelector('.submitBudget');
+// let budgetA = document.querySelector(".budgetAmount" );
+// let budgetEdit = document.querySelector(".editBudget");
+// let expensesAmount= document.querySelector('.purchased');
 
-budgetEdit.style.visibility = "hidden";
+document.querySelector(".editBudget").style.visibility = "hidden";
 
-budgetButton.addEventListener('click', function(){
-    budgetA.style.visibility = "hidden";
-    budgetEdit.style.visibility = "visible";
-    budgetButton.style.visibility = "hidden";
-    document.querySelector('.displayTotal').style.visibility="visible"
+document.querySelector('.submitBudget').addEventListener('click', function(){
+    document.querySelector(".budgetAmount" ).style.visibility = "hidden";
+    document.querySelector(".editBudget").style.visibility = "visible";
+    document.querySelector('.submitBudget').style.visibility = "hidden";
+    document.querySelector('.displayTotal').style.visibility="visible";
     document.querySelector('.displayTotal').style.color = "yellow";
     let inputValue = document.querySelector('.budgetAmount').value;
     let display = document.querySelector('.displayTotal');
     display.innerHTML =`$ ${inputValue}`;
 
-});
+document.querySelector(".editBudget").addEventListener('click', function() {
+        document.querySelector(".budgetAmount" ).style.visibility = "visible";
+        document.querySelector(".editBudget").style.visibility = "hidden";
+        document.querySelector('.submitBudget').style.visibility = "visible";
+        document.querySelector(".displayTotal").style.visibility = "hidden";
+        document.querySelector(".budgetAmount" ).value = "none";
+    });
 
-function expensesAmount(e) {
-  e.preventDefault();
+});
+document.getElementById("purchase").addEventListener('click',function(){
   let expenses = document.getElementById("expenses").value;
   let purchaseAmount = document.getElementById("purchaseAmount").value;
   let listOfItems = document.getElementById("listOfItems");
@@ -36,5 +42,42 @@ function expensesAmount(e) {
   } else if (expenses === "Clothing") {
     item3.append(` Clothing amount: $ ${purchaseAmount}`);
   }
-}
+});
+let entertainmentTotal = 0.00;
+let clothingTotal = 0.00;
+let foodTotal = 0.00;
+let billsTotal = 0.00;
+document.getElementById("purchase").addEventListener("click", function() {
+    let expenses = document.getElementById("expenses").value;
+    let purchaseAmount = document.getElementById("purchaseAmount").value;
+    let entertainment = document.getElementById("entertainment")
+    let food = document.getElementById("food")
+    let clothing = document.getElementById("clothing")
+    let bills = document.getElementById("bills")
+    if (expenses === "Entertainment") {
+        entertainmentTotal = entertainmentTotal + parseFloat(purchaseAmount);
+        entertainment.innerHTML="Entertainment: $" + entertainmentTotal;
+      } else if (expenses === "Food") {
+        foodTotal = foodTotal + parseFloat(purchaseAmount);
+        food.innerHTML="Food: $" + foodTotal;
+      } else if (expenses === "Bills") {
+        billsTotal = billsTotal + parseFloat(purchaseAmount);
+        bills.innerHTML="Bills: $" + billsTotal;
+      } else if (expenses === "Clothing") {
+        clothingTotal = clothingTotal + parseFloat(purchaseAmount);
+       clothing.innerHTML="Clothing: $" + clothingTotal;
+      }
+});
+
+
+
+
+
+
+
+
+
+
+
+
 
