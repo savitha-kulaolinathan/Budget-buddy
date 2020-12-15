a5eb4c0e5456d294a3f1338b5d0b311414ba881b
Savitha <savidak@gmail.com>
2020-12-14 20:28:48 -0500

    recent changes

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ document.querySelector(".editBudget").addEventListener('click', function() {
 });
 
 document.getElementById("purchase").addEventListener('click',function(){
- 
+ let stuff = document.getElementById("stuff").value
   let expenses = document.getElementById("expenses").value;
   let purchaseAmount = document.getElementById("purchaseAmount").value;
   let listOfItems = document.getElementById("listOfItems"); 
@@ -38,19 +38,19 @@ document.getElementById("purchase").addEventListener('click',function(){
 
   if (expenses === "Entertainment" && balanceCheck >= 0) {
     let item1 = document.createElement("div");
-    item1.innerText = `Entertainment: $ ${purchaseAmount}`;
+    item1.innerText = `${stuff}: $ ${purchaseAmount}`;
     listOfItems.append(item1);   
   } else if (expenses === "Food" && balanceCheck >= 0) {
     let item2 = document.createElement("div");
-    item2.innerText = `Food: $ ${purchaseAmount}`;
+    item2.innerText = `${stuff}: $ ${purchaseAmount}`;
     listOfItems.append(item2);
   } else if (expenses === "Bills" && balanceCheck >= 0) {
     let item4 = document.createElement("div");
-    item4.innerText = ` Billing: $ ${purchaseAmount}`;
+    item4.innerText = `${stuff}: $ ${purchaseAmount}`;
     listOfItems.append(item4);
   } else if (expenses === "Clothing" && balanceCheck >= 0) {
     let item3 = document.createElement("div");
-    item3.innerText = ` Clothing: $ ${purchaseAmount}`;
+    item3.innerText = ` ${stuff}: $ ${purchaseAmount}`;
     listOfItems.append(item3);
   }
     if (purchaseAmount != 0) {
@@ -98,6 +98,35 @@ document.getElementById("purchase").addEventListener("click", function() {
         clothingTotal = clothingTotal + parseFloat(purchaseAmount);
        clothing.innerHTML="Clothing: $" + clothingTotal;
       }
+      var ctx = document.getElementById("chart").getContext("2d");
+      var chart = new Chart(ctx, {
+        type: "pie",
+        data: {
+          labels: ["Entertainment", "Clothing", "Food", "Bills"],
+          datasets: [
+            {
+              label: "Spending by Category",
+              data: [entertainmentTotal, clothingTotal, foodTotal, billsTotal],
+              backgroundColor: [
+                "rgba(255, 99, 132, 1)",
+                "rgba(54, 162, 235, 1)",
+                "rgba(255, 206, 86, 1)",
+                "rgba(75, 192, 192, 1)",
+              ],
+              borderColor: [
+                "rgba(255, 99, 132, 1)",
+                "rgba(54, 162, 235, 1)",
+                "rgba(255, 206, 86, 1)",
+                "rgba(75, 192, 192, 1)",
+              ],
+              borderWidth: 1,
+            },
+          ],
+        },
+        options: {
+         
+        },
+      });
 });
 
 
