24bbdbb104114696509fa361bf3331f10805a699
Savitha <savidak@gmail.com>
2020-12-17 16:35:40 -0500

    final code

--- script.js
+++ script.js
@@ -1,133 +1,97 @@
-let updateBalance= document.getElementById('updateBalance');
-let  availableAmt = document.getElementById('spendLabel');
-
+let updateBalance = document.getElementById("updateBalance");
+let availableAmt = document.getElementById("spendLabel");
+let entertainmentTotal = 0.0;
+let clothingTotal = 0.0;
+let foodTotal = 0.0;
+let billsTotal = 0.0;
 document.querySelector(".editBudget").style.visibility = "hidden";
-document.querySelector('.submitBudget').addEventListener('click', function(e){
+document.querySelector(".submitBudget").addEventListener("click", function (e) {
   e.preventDefault();
-    document.querySelector(".budgetAmount" ).style.visibility = "hidden";
-    document.querySelector(".editBudget").style.visibility = "visible";
-    document.querySelector('.submitBudget').style.visibility = "hidden";
-    document.querySelector('.displayTotal').style.visibility="visible";
-    document.querySelector('.displayTotal').style.color = "yellow";
-    let inputValue = document.querySelector('.budgetAmount').value;
-    let display = document.querySelector('.displayTotal');
-    let indiv = document.getElementById('indiv');
-    
-    indiv.style.color="yellow"
-    indiv.style.fontSize ="40";
-    display.innerHTML =`$ ${inputValue}`;
-      let amt =document.getElementById("amt");
-     
-       amt.style.color ="yellow";
-       amt.style.fontSize= "40";
-     availableAmt.style.color ="yellow" 
-     availableAmt.style.fontSize ="40px";
-     if (inputValue === "") {
-       alert ("Enter the value");
-     } else{
-      amt.innerHTML =parseInt(amt.innerHTML)+parseInt(inputValue);
-      updateBalance.innerHTML =  parseInt(updateBalance.innerHTML) + parseInt(inputValue); 
-
-     }
-    
-      
-   console.log(updateBalance.innerHTML);
-     updateBalance.style.color = "yellow"
-     updateBalance.style.fontSize="40px";
-     
-   
-   
-
-
-
-document.querySelector(".editBudget").addEventListener('click', function(e) {
-  e.preventDefault();
-        document.querySelector(".budgetAmount" ).style.visibility = "visible";
-        document.querySelector(".editBudget").style.visibility = "hidden";
-        document.querySelector('.submitBudget').style.visibility = "visible";
-        document.querySelector(".displayTotal").style.visibility = "hidden";
-        document.querySelector(".budgetAmount" ).value = "none";
-        
-    });
+  document.querySelector(".budgetAmount").style.visibility = "hidden";
+  document.querySelector(".editBudget").style.visibility = "visible";
+  document.querySelector(".submitBudget").style.visibility = "hidden";
+  document.querySelector(".displayTotal").style.visibility = "visible";
+  document.querySelector(".displayTotal").style.color = "yellow";
+  let inputValue = document.querySelector(".budgetAmount").value;
+  let display = document.querySelector(".displayTotal");
+  let indiv = document.getElementById("indiv");
+  let amt = document.getElementById("amt");
+  indiv.style.color = "yellow";
+  indiv.style.fontSize = "40";
+  amt.style.color = "yellow";
+  amt.style.fontSize = "40";
+  availableAmt.style.color = "yellow";
+  availableAmt.style.fontSize = "40px";
+  updateBalance.style.color = "yellow";
+  updateBalance.style.fontSize = "40px";
+  display.innerHTML = `$ ${inputValue}`;
+  if (inputValue === "") {
+    alert("Enter the value");
+  } else {
+    amt.innerHTML = `${parseFloat(amt.innerHTML) + parseFloat(inputValue)}`;
+    updateBalance.innerHTML = `${
+      parseFloat(updateBalance.innerHTML) + parseFloat(inputValue)
+    }`;
+  }
+  document.querySelector(".editBudget").addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(".budgetAmount").style.visibility = "visible";
+    document.querySelector(".editBudget").style.visibility = "hidden";
+    document.querySelector(".submitBudget").style.visibility = "visible";
+    document.querySelector(".displayTotal").style.visibility = "hidden";
+    document.querySelector(".budgetAmount").value = "none";
+  });
 });
 
-document.getElementById("purchase").addEventListener('click',function(){
- let stuff = document.getElementById("stuff").value
+document.getElementById("purchase").addEventListener("click", function (e) {
+  e.preventDefault();
+  let stuff = document.getElementById("stuff").value;
   let expenses = document.getElementById("expenses").value;
   let purchaseAmount = document.getElementById("purchaseAmount").value;
-  let listOfItems = document.getElementById("listOfItems"); 
-  let balanceCheck= 0;
-   balanceCheck = parseFloat(updateBalance.innerHTML)-parseFloat(purchaseAmount);
-   ;
-
+  let listOfItems = document.getElementById("listOfItems");
+  let balanceCheck = 0.0;
+  balanceCheck =
+    parseFloat(updateBalance.innerHTML) - parseFloat(purchaseAmount);
+  if (purchaseAmount != 0) {
+    if (balanceCheck >= 0.0) {
+      updateBalance.innerHTML = (
+        parseFloat(updateBalance.innerHTML) - parseFloat(purchaseAmount)
+      ).toFixed(2);
+    } else {
+      alert("Do not have sufficient money!");
+      availableAmt.style.color = "red";
+      availableAmt.style.fontSize = "40px";
+      updateBalance.style.color = "red";
+      updateBalance.style.fontSize = "40px";
+      updateBalance.innerHTML = parseFloat(updateBalance.innerHTML).toFixed(2);
+      console.log(updateBalance.innerHTML);
+    }
+  }
 
   if (expenses === "Entertainment" && balanceCheck >= 0) {
     let item1 = document.createElement("div");
-    
-    
     item1.innerHTML = `${stuff}: $ ${purchaseAmount}`;
-    listOfItems.append(item1);   
+    listOfItems.append(item1);
+    item1.append(itemValue1);
+    entertainmentTotal = entertainmentTotal + parseFloat(purchaseAmount);
   } else if (expenses === "Food" && balanceCheck >= 0) {
     let item2 = document.createElement("div");
-    item2.innerText = `${stuff}: $ ${purchaseAmount}`;
+    item2.innerHTML = `${stuff}: $ ${purchaseAmount}`;
     listOfItems.append(item2);
+    foodTotal = foodTotal + parseFloat(purchaseAmount);
   } else if (expenses === "Bills" && balanceCheck >= 0) {
     let item4 = document.createElement("div");
-    item4.innerText = `${stuff}: $ ${purchaseAmount}`;
+    item4.innerHTML = `${stuff}: $ ${purchaseAmount}`;
+    billsTotal = billsTotal + parseFloat(purchaseAmount);
     listOfItems.append(item4);
   } else if (expenses === "Clothing" && balanceCheck >= 0) {
     let item3 = document.createElement("div");
-    item3.innerText = ` ${stuff}: $ ${purchaseAmount}`;
+    item3.innerHTML = ` ${stuff}: $ ${purchaseAmount}`;
     listOfItems.append(item3);
+    clothingTotal = clothingTotal + parseFloat(purchaseAmount);
   }
-    if (purchaseAmount != 0) {
-      if (balanceCheck >= 0 ) {
-      updateBalance.innerHTML -= ` ${purchaseAmount}`;
-      
-     } 
-     else{
-      alert ("Do not have sufficient money!");
-      availableAmt.style.color ="red" 
-      availableAmt.style.fontSize= "40px";
-      updateBalance.style.color ="red" 
-      updateBalance.style.fontSize = "40px";
-      updateBalance.innerHTML =  parseInt(updateBalance.innerHTML) + parseInt(inputValue); 
-      // updateBalance.textContent = "Insufficient Balance";
-      
-     }       
 
-     }
-  
-});
-let entertainmentTotal = 0.00;
-let clothingTotal = 0.00;
-let foodTotal = 0.00;
-let billsTotal = 0.00;
-document.getElementById("purchase").addEventListener("click", function() {
-    let expenses = document.getElementById("expenses").value;
-    let purchaseAmount = document.getElementById("purchaseAmount").value;
-    let entertainment = document.getElementById("entertainment")
-    let food = document.getElementById("food")
-    let clothing = document.getElementById("clothing")
-    let bills = document.getElementById("bills")
-    
-    balanceCheck = parseFloat(updateBalance.innerHTML)-parseFloat(purchaseAmount);
- 
-   
-    if (expenses === "Entertainment" &&  balanceCheck >= 0) {
-        entertainmentTotal = entertainmentTotal + parseFloat(purchaseAmount);
-        //  entertainment.innerHTML="Entertainment: $" + entertainmentTotal;
-      } else if (expenses === "Food" &&  balanceCheck >= 0) {
-        foodTotal = foodTotal + parseFloat(purchaseAmount);
-        // food.innerHTML="Food: $" + foodTotal;
-      } else if (expenses === "Bills" && balanceCheck >= 0) {
-        billsTotal = billsTotal + parseFloat(purchaseAmount);
-        // bills.innerHTML="Bills: $" + billsTotal;
-      } else if (expenses === "Clothing" && balanceCheck >= 0) {
-        clothingTotal = clothingTotal + parseFloat(purchaseAmount);
-      //  clothing.innerHTML="Clothing: $" + clothingTotal;
-      }
-      var ctx = document.getElementById("chart").getContext("2d");
+  var ctx = document.getElementById("chart").getContext("2d");
   var chart = new Chart(ctx, {
     type: "pie",
     data: {
@@ -141,32 +105,19 @@ document.getElementById("purchase").addEventListener("click", function() {
             "rgba(255,127,80, 1)",
             "rgba(255, 206, 86, 1)",
             "rgba(75, 192, 192, 1)",
+            "rgba(234, 100, 191, 1)",
           ],
           borderColor: [
             "rgba(255, 99, 132, 1)",
             "rgba(255,127,80, 1)",
             "rgba(255, 206, 86, 1)",
             "rgba(75, 192, 192, 1)",
+            "rgba(234, 100, 191, 1)",
           ],
           borderWidth: 1,
         },
       ],
     },
-    options: {
-     
-    },
+    options: {},
   });
 });
-
-
-
-
-
-
-
-
-  
-
-
-
-
